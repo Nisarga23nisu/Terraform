168d03eabbd6f2c576e6af0651e3e7d835f6cfec
Nisarga23nisu <156158786+Nisarga23nisu@users.noreply.github.com>
2025-01-08 14:50:37 +0530

    Update provi.tf

--- Terra-7/provi.tf
+++ Terra-7/provi.tf
@@ -12,6 +12,30 @@ provider "aws" {
     region = var.aws-region
 }
 
+##resource "aws_security_group" "allowing_ssh" {
+  name        = "allowing_ssh"
+  description = "allowing_ssh"
+
+    ingress {
+        description = "Inbound Traffic"    
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Outbound traffic"
+        from_port        = 0
+        to_port          = 65535
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    tags = {
+    Name = "allowing_ssh"
+    }
+}###
+
 resource "aws_instance" "ec2-2"{
     ami = var.ami-id
     instance_type = var.instance-type
